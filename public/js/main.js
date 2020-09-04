a7d1bfd220bf4ee37d93883a0a89acdd61863e3b
Dare Lawal <lawaldare@gmail.com>
2020-09-04 14:39:19 +0100

    finally done

---
+++ public/js/main.js
@@ -0,0 +1,98 @@
+const api = {
+  key: "1f851222062e779b9c6638e3f1739e8e",
+  base: "https://api.openweathermap.org/data/2.5/weather",
+}
+
+const loader = document.getElementById('loader');
+const main = document.getElementById('main');
+const form = document.querySelector('.form');
+
+loader.hidden = true;
+let searchTerm = '';
+
+
+
+getSavedData();
+
+async function fetchWeather(city){
+main.style.visibility = 'hidden';
+loader.hidden = false;
+const API = `${api.base}?q=${city}&units=metric&appid=${api.key}`;
+  try {
+    const res = await fetch(API);
+    const data = await res.json();
+    if(data.cod === 200) {
+      loader.hidden = true;
+      main.style.visibility = 'visible';
+      localStorage.setItem('data', JSON.stringify(data));
+      displayResults(data);
+    } else {
+      loader.hidden = true;
+      alert(data.message.toUpperCase());
+      getSavedData();
+    }
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+
+function getWeather(e){
+  e.preventDefault();
+  searchTerm = e.srcElement[0].value;
+  fetchWeather(searchTerm);
+  e.srcElement[0].value = "";
+}
+
+function getSavedData(){
+    if(JSON.parse(localStorage.getItem('data'))){
+      main.style.visibility = 'visible';
+     displayResults(JSON.parse(localStorage.getItem('data')))
+  }
+}
+
+form.addEventListener('submit', getWeather);
+
+
+function displayResults (weather) {
+  let city = document.querySelector('.city');
+  city.innerText = `${weather.name} Weather Report`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let icon = document.querySelector('.current .icon');
+  icon.src = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${weather.main.temp}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].description;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max}°c`;
+}
+
+
+
+function dateBuilder (d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
+
+// PWA
+
+if("serviceWorker" in navigator){
+  navigator.serviceWorker.register('/sw.js').then(res => {
+    console.log(res);
+  }).catch(error => console.log(error));
+}
---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+const assets = [
+  '/',
+  './index.html',
+  './css/main.css',
+  './js/main.js',
+  'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700&display=swap',
+  'https://fonts.googleapis.com/css2?family=Satisfy&display=swap',
+  './assets/loader.svg',
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil( caches.open(staticCacheName).then(cache => {
+    cache.addAll(assets);
+  }))
+});
+
+//activate the service worker
+self.addEventListener('activate', e => {
+  e.waitUntil( caches.keys().then(keys => {
+    return Promise.all(keys.filter(key => key !== staticCacheName).map(key => caches.delete(key)))
+  }))
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cacheRequest => {
+    return cacheRequest || fetch(e.request).then(fetchRes => {
+      return caches.open(dynamicCacheName).then(cache => {
+        cache.put(e.request.url, fetchRes.clone());
+        return fetchRes;
+      })
+    })
+  }))
+})
